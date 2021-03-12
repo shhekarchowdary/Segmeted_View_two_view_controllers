bbff66a66ec5f1a1c1f9bf27bd416f8bae217fa8
somasekharanaparthi <shhekarchowdary@gmail.com>
2021-03-13 00:13:51 +0530

    with same username no register

--- two_view_controllers/SecondViewController.swift
+++ two_view_controllers/SecondViewController.swift
@@ -14,10 +14,14 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var userSegment: UISegmentedControl!
     
     
     @IBOutlet weak var register: UIButton!
     @IBOutlet weak var login: UIButton!
+    
+    var loginUser = ""
+    var userPassword = ["user" : "user"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,55 +29,76 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    var loginUser = ""
-    var userPassword = ["user1":"12345"]
-    
-    @IBAction func newUserPressed(_ sender: Any) {
-        userNameLabel.isHidden = false
-        passwordLabel.isHidden = false
-        userName.isHidden = false
-        password.isHidden = false
-        
-        userName.text = ""
-        password.text = ""
-        
-        register.isHidden = false
-        login.isHidden = true
-        //print("Login:\(login.isHidden)")
-       // print("Regis:\(password.isHidden)")
-        
-        message.text = "Please create credentials to Register"
-        message.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-    }
-    
-    @IBAction func exisUserPressed(_ sender: Any) {
-        userNameLabel.isHidden = false
-        passwordLabel.isHidden = false
-        userName.isHidden = false
-        password.isHidden = false
-        
-        userName.text = ""
-        password.text = ""
-        
-        register.isHidden = true
-        login.isHidden = false
-        //print("Login:\(login.isHidden ? "visible" : "not visible")")
-        //print("Regis:\(password.isHidden ? "visible" : "not visible")")
-        
-        message.text = "Please give credentials to Login"
-        message.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+    @IBAction func userSegmentation(_ sender: UISegmentedControl)
+    {
+        if sender.selectedSegmentIndex == 0{
+            sender.selectedSegmentTintColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+            userNameLabel.isHidden = false
+            passwordLabel.isHidden = false
+            userName.isHidden = false
+            password.isHidden = false
+            
+            userName.text = ""
+            password.text = ""
+            
+            register.isHidden = false
+            login.isHidden = true
+            //print("Login:\(login.isHidden)")
+           // print("Regis:\(password.isHidden)")
+            
+            message.text = "Please create credentials to Register"
+            message.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        }else if sender.selectedSegmentIndex == 1{
+            sender.selectedSegmentTintColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+            userNameLabel.isHidden = false
+            passwordLabel.isHidden = false
+            userName.isHidden = false
+            password.isHidden = false
+            
+            userName.text = ""
+            password.text = ""
+            
+            register.isHidden = true
+            login.isHidden = false
+            //print("Login:\(login.isHidden ? "visible" : "not visible")")
+            //print("Regis:\(password.isHidden ? "visible" : "not visible")")
+            
+            message.text = "Please give credentials to Login"
+            message.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        }else if sender.selectedSegmentIndex == 2{
+            userName.text = ""
+            password.text = ""
+            userNameLabel.isHidden = true
+            passwordLabel.isHidden = true
+            userName.isHidden = true
+            password.isHidden = true
+            register.isHidden = true
+            login.isHidden = true
+            
+            message.text = ""
+        }
     }
     
     @IBAction func registerPressed(_ sender: Any) {
+        print("register User Pressed")
         if userName.text != ""{
-            if password.text != ""{
-                userPassword[userName.text!] = password.text!
-                userName.text = ""
-                password.text = ""
-                message.text = "Registration Succesful Please Login"
-                message.textColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-                register.isHidden = true
-                login.isHidden = false
+            if password.text != ""
+            {
+                let gUser = userName.text!
+                let gPassword = password.text!
+                for user in userPassword.keys
+                {
+                    if user == gUser {
+                        message.text = "User Name Already Exists Please Choose other"
+                        message.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+                        break
+                    }else{
+                        userPassword[gUser] = gPassword
+                        print(userPassword)
+                        message.text = "Registration Succesful Please Login"
+                        message.textColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+                    }
+                }
             }else{
                 message.text = "Please enter Password"
                 message.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
@@ -89,19 +114,18 @@ class SecondViewController: UIViewController {
         loginUser = ""
         if userName.text != ""{
             if password.text != ""{
+                let gUser = userName.text!
+                let gPassword = password.text!
                 for user in userPassword.keys{
-                    if user == userName.text {
-                        if userPassword[user] == password.text{
+                    if user == gUser {
+                        if userPassword[user] == gPassword{
                             loginUser = userName.text!
                             message.text = "Login Succesfull"
                             message.textColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-                            userNameLabel.isHidden = true
-                            passwordLabel.isHidden = true
-                            userName.isHidden = true
-                            password.isHidden = true
-                            register.isHidden = true
-                            login.isHidden = true
-                            
+                            userSegment.isSelected = true
+                            userName.text = ""
+                            password.text = ""
+                            break
                         }else{
                             message.text = "Wrong Password"
                             message.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
@@ -119,20 +143,6 @@ class SecondViewController: UIViewController {
             message.text = "Please enter User Name"
             message.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         }
-    }
-    
-    
-    @IBAction func clearPressed(_ sender: Any) {
-        userName.text = ""
-        password.text = ""
-        userNameLabel.isHidden = true
-        passwordLabel.isHidden = true
-        userName.isHidden = true
-        password.isHidden = true
-        register.isHidden = true
-        login.isHidden = true
-        
-        message.text = ""
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
